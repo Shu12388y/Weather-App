2f18619bf15dcb1f0b8ee9f7595e78e6bf6a8535
shubham paul <91714143+Shu12388y@users.noreply.github.com>
2022-12-27 15:21:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e041d4411amsh47ce7147cafdbdep1d5421jsn738909aac395',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+
+
+const getCity = (city) => {
+
+	
+	cityName.innerHTML = city
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+		.then(response => response.json())
+		.then((response) => {
+
+			console.log(response)
+
+			cloud_pct.innerHTML = response.cloud_pct
+			temp.innerHTML = response.temp
+			feels_like.innerHTML = response.feels_like
+			humidity.innerHTML = response.humidity
+			min_temp.innerHTML = response.min_temp
+			max_temp.innerHTML = response.max_temp
+			wind_speed.innerHTML = response.wind_speed
+			wind_degrees.innerHTML = response.wind_degrees
+			sunrise.innerHTML = response.sunrise
+			sunset.innerHTML = response.sunset
+
+
+
+		})
+
+		.catch(err => console.error(err))
+}
+
+
+
+Submit.addEventListener("click", e => {
+	e.preventDefault();
+	getCity(city.value)
+})
+
+
+
